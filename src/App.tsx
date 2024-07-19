49df60ccd37b17d091a99c745d53e94c8d705ece
aschneidman28 <75656492+aschneidman28@users.noreply.github.com>
2024-07-19 09:20:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import CaterpillarGame from './caterpillar-game-react.ts';  // Add the .tsx extension
+import CaterpillarGame from './caterpillar-game-react.tsx';  // Add the .tsx extension
 
 function App() {
   return (
