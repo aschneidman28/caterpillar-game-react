cd32b55a11f8bf1f753d17390707e8a0b2fbc548
aschneidman28 <75656492+aschneidman28@users.noreply.github.com>
2024-07-22 22:21:01 -0400

    Update caterpillar-game-react.tsx

--- src/caterpillar-game-react.tsx
+++ src/caterpillar-game-react.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './CaterpillarGame.css'; // We'll create this CSS file next
 
 // Import the JSON data
 import weeklyClues from './weekly-clues.json';
