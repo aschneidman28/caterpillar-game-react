7774d2a695a90fab461eeee78f0b0e94db4b87ca
aschneidman28 <75656492+aschneidman28@users.noreply.github.com>
2024-07-22 20:54:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Caterpillar Game</h1>
+        <h1></h1>
         <CaterpillarGame />
       </header>
     </div>
